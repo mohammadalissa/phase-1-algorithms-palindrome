b9d8791f844c400ec6cecfacf123087248711cb9
Mohammad Al Issa <mohammed.alissa627@gmail.com>
2023-10-14 19:02:39 +0300

    completed

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let Nword = ''
+  for(let i = word.length - 1; i >= 0;i--){
+    Nword += word[i]
+
+  }
+  return Nword === word
 }
 
 /* 
-  Add your pseudocode here
+   iterate over each letter of the string:
+  initialize empty string called Nword
+	      push the last letter to the new string       
+  check if the word equals the new word:
 */
 
 /*
   Add written explanation of your solution here
+  we need a function that check if the string it takes can be read from first to last and from last to first with the same word 
 */
 
 // You can run `node index.js` to view these console logs
@@ -18,8 +27,19 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("tamat"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("rubber"));
+
 }
 
 module.exports = isPalindrome;
